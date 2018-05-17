3c0417b88c53f5444bbfdf06ae7ddcefcbda35e9
Tobi Wulff <tobi@swulff.de>
2018-05-17 13:20:41 +0200

    Add more shifting between modes

--- footpedal.py
+++ footpedal.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import time
 from rtmidi import RtMidiIn, MidiMessage
 from subprocess import call
 from mod_host import ModHostClient
@@ -14,6 +15,7 @@ def preset_cb(preset_number):
         info = c.get_plugin_info('MultiChorus')
         for p in info['parameters']:
             print('{}: {} to {}'.format(p['symbol'], p['minimum'], p['maximum']))
+        c.remove_all_plugins()
         c.load_preset_string('MultiChorus[0 0 0 0]')
         print(c.get_all_parameters(0, info))
         c.set_parameter(0, 'mod_rate', 1.8)
@@ -21,29 +23,41 @@ def preset_cb(preset_number):
         info = c.get_plugin_info('Reverb')
         for p in info['parameters']:
             print('{}: {} to {}'.format(p['symbol'], p['minimum'], p['maximum']))
+        c.remove_all_plugins()
         c.load_preset_string('Compressor[0 0],Reverb[0 0 0 0]')
         print(c.get_all_parameters(1, info))
         c.set_parameter(1, 'room_size', 3)
+    print(subprocess.check_output(['jack_lsp', '-c']))
 
 
 def looper_cb(enable):
     if enable:
         print('Loading the looper ...')
+        call(['/root/guitar.sh', 'stop'])
+        time.sleep(1)
         call(['/root/looper.sh'])
     else:
         print('Killing the looper.')
         call(['killall', 'sooperlooper'])
+        time.sleep(1)
+        call(['/root/guitar.sh', 'start'])
+
+
+looper_enabled = False
 
 
 def midi_message_cb(msg):
+    global looper_enabled
+
     channel, cc, value = msg.getChannel(), msg.getControllerNumber(), msg.getControllerValue()
     print('{} -> {}'.format(cc, '1' if value > 0 else '0'))
 
     if value == 127:
-        if 60 <= cc <= 63:
+        if 60 <= cc <= 61:
             preset_cb(cc - 60)
-        elif 64 <= cc <= 65:
-            looper_cb(enable=cc == 64)
+        elif cc  == 65:
+            looper_enabled = not looper_enabled
+            looper_cb(enable=looper_enabled)
 
 
 def main():
